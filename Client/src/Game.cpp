6e9a1e509494e51ad7e1b6ccca9dab51b7e2dffd
HaiLe <dark.hades.1102@gmail.com>
2017-08-22 14:35:36 +0900

    Draw

--- Client/src/Game.cpp
+++ Client/src/Game.cpp
@@ -40,10 +40,14 @@ namespace app {
     }
   }
 
+// #undef GAMEBOARD_ONEOBJ_SIZE
+// #define GAMEBOARD_ONEOBJ_SIZE 5
+
   void Game::drawGameBoard()
   {
-    // Header
-    auto drawBarrier = [=](int len){
+    assert((GAMEBOARD_ONEOBJ_SIZE % 2) != 0);
+    // Barrier
+    auto drawBarrier = [](int len){
       for (size_t i = 0; i < GAMEBOARD_COLS; i++)
       {
         std::cout << '+';
@@ -53,20 +57,35 @@ namespace app {
       std::cout << std::endl;
     };
 
-    assert((GAMEBOARD_ONEOBJ_SIZE % 2) != 0);
+    // Body
+    auto drawLine = [](const std::string s){
+        std::cout << '|';
+        std::cout << std::string(GAMEBOARD_ONEOBJ_SIZE / 2, ' ');
+        std::cout << s;
+        std::cout << std::string(GAMEBOARD_ONEOBJ_SIZE / 2, ' ');
+    };
 
     for (size_t x = 0; x < m_gameBoard.size(); x++)
     {
       drawBarrier(GAMEBOARD_ONEOBJ_SIZE);
+      // for (size_t y = 0; y < m_gameBoard.at(x).size(); y++)
+      // {
+      //   drawBody(" ");
+      // }
+      // std::cout << '|';
+      // std::cout << std::endl;
       for (size_t y = 0; y < m_gameBoard.at(x).size(); y++)
       {
-        std::cout << '|';
-        std::cout << std::string(GAMEBOARD_ONEOBJ_SIZE / 2, ' ');
-        std::cout << m_gameBoard.at(x).at(y);
-        std::cout << std::string(GAMEBOARD_ONEOBJ_SIZE / 2, ' ');
+        drawLine(std::to_string(m_gameBoard.at(x).at(y)));
       }
       std::cout << '|';
       std::cout << std::endl;
+      // for (size_t y = 0; y < m_gameBoard.at(x).size(); y++)
+      // {
+      //   drawBody(" ");
+      // }
+      // std::cout << '|';
+      // std::cout << std::endl;
     }
     drawBarrier(GAMEBOARD_ONEOBJ_SIZE);
   }
