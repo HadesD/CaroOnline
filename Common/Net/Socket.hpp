2e800879d8befae9daeb26c39d3adf2290000d51
HaiLe <dark.hades.1102@gmail.com>
2017-09-13 18:41:28 +0900

    Socket class

---
+++ Common/Net/Socket.hpp
@@ -0,0 +1,16 @@
+#ifndef COMMON_NET_SOCKET
+#define COMMON_NET_SOCKET
+// #include ""
+
+namespace common {
+
+  class Socket
+  {
+    public:
+      Socket();
+      ~Socket();
+  };
+
+}
+
+#endif
